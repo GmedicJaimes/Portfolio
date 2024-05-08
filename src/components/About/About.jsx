9399f863d0defd1b432a2f779f589f057b492378
Gulliano Jaimes <88950788+GmedicJaimes@users.noreply.github.com>
2024-05-08 12:38:06 -0500

    tamaño de botones arreglado

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -21,7 +21,7 @@ const About = () => {
             Hola
             <span>,</span> soy Gulliano<span>!</span>
           </h1>
-          <h3>Desarrollador de Software</h3>
+          {/* <h3>Desarrollador de Software</h3> */}
         </div>
         <p>
           +1 año de experiencia.{" "}
@@ -29,38 +29,46 @@ const About = () => {
             Desarrollador de Software y Administrador de Empresas.
           </span>{" "}
           <span>
-            Especializado en el mantenimiento y en la construcion de
-            aplicaciones web dinamicas y efecientes, apasionado por la
-            tecnologia y la innovacion.
+            Especializado en la construción y mantenimiento de aplicaciones web
+            dinamicas y efecientes,
           </span>{" "}
           apasionado por la tecnologia y la innovacion.{" "}
         </p>
 
         <div className={style.buttonsAbout}>
-          <a
-            href="https://www.linkedin.com/in/gulliano-jaimes/"
-            target="_blank"
-          >
-            <img
-              src="https://cdn-icons-png.flaticon.com/128/61/61109.png"
-              alt=""
-              loading="lazy"
-            />{" "}
-          </a>
-          <a href="https://github.com/GmedicJaimes" target="_blank">
-            <img
-              src="https://cdn-icons-png.flaticon.com/128/733/733609.png"
-              alt=""
-              loading="lazy"
-            />
-          </a>
-          <a href={CV} download={""}>
-            <img
-              src="https://cdn-icons-png.flaticon.com/128/909/909263.png"
-              alt=""
-              loading="lazy"
-            />
-          </a>
+          <button>
+            <a
+              href="https://www.linkedin.com/in/gulliano-jaimes/"
+              target="_blank"
+            >
+              <img
+                src="https://cdn-icons-png.flaticon.com/128/61/61109.png"
+                alt=""
+                loading="lazy"
+              />
+              linkedin
+            </a>
+          </button>
+          <button>
+            <a href="https://github.com/GmedicJaimes" target="_blank">
+              <img
+                src="https://cdn-icons-png.flaticon.com/128/733/733609.png"
+                alt=""
+                loading="lazy"
+              />{" "}
+              Github
+            </a>
+          </button>
+          <button>
+            <a href={CV} download={""}>
+              <img
+                src="https://cdn-icons-png.flaticon.com/128/909/909263.png"
+                alt=""
+                loading="lazy"
+              />{" "}
+              Curriculum
+            </a>
+          </button>
         </div>
       </div>
     </section>
