7a1125361492a7f5757e7f4786f1a0fa10020ecd
Gulliano Jaimes <88950788+GmedicJaimes@users.noreply.github.com>
2024-05-11 22:07:09 -0500

    timeline terminado

--- src/components/Experiencia/Experiencia.jsx
+++ src/components/Experiencia/Experiencia.jsx
@@ -9,32 +9,37 @@ const Experiencia = () => {
 
         <section className="contentsExperiencia">
           <div className="box">
-            <div>
+            <div className="companyName">
               <h4>Analista de Soporte Técnico y Documentación</h4>
               <h3>Universidad Francisco de Paula Santander</h3>
               <h2>Enero 2024 - Presente</h2>
             </div>
-            <div>
+            <div className="descriptionCompany">
               <p></p>
             </div>
           </div>
           <div className="box">
-            <div>
+            <div className="companyName">
               <h4>Asistente de Enseñanza en Desarrollo Full Stack </h4>
               <h3>SoyHenry Bootcamp</h3>
               <h2>Julio 2023 - Agosto 2023</h2>
             </div>
-            <div>
-              <p></p>
+            <div className="descriptionCompany">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam
+                molestias velit, repudiandae obcaecati reiciendis eaque quaerat
+                optio sapiente provident incidunt illo nulla ipsam soluta sunt
+                quasi cum magni autem mollitia!
+              </p>
             </div>
           </div>
           <div className="box">
-            <div>
+            <div className="companyName">
               <h4>Jefe de Logistica</h4>
               <h3>Villaceramica</h3>
               <h2>Julio 2022 - Abril 2023</h2>
             </div>
-            <div>
+            <div className="descriptionCompany">
               <p></p>
             </div>
           </div>
